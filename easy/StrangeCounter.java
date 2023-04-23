8cac4c985cd46742e617555bdcbcd411bfb448f1
rsmciftci <rsmciftci@gmail.com>
2023-04-23 17:42:54 +0300

    Strange Counter

---
+++ easy/StrangeCounter.java
@@ -0,0 +1,25 @@
+package easy;
+
+class StrangeCounter {
+
+    // https://www.hackerrank.com/challenges/strange-code/problem?isFullScreen=true
+
+    public static long strangeCounter(long t) {
+        // Write your code here
+
+        long curr = 3;
+        while (t > curr) {
+            t -= curr;
+            curr *= 2;
+        }
+        return (curr-t+1);
+
+
+    }
+
+    public static void main(String[] args) {
+            long t = 5L;
+            System.out.println(strangeCounter(t));
+
+    }
+}
