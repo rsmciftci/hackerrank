088267d00a8fe1aa487ccbf8826fbb0c8b6d5353
rsmciftci <rsmciftci@gmail.com>
2023-04-27 20:21:42 +0300

    Game of Thrones I

---
+++ easy/strings/GameOfThronesI.java
@@ -0,0 +1,47 @@
+package easy.strings;
+
+import java.util.*;
+
+class GameOfThronesI {
+
+    // Anagram of Palindrome
+
+    // can string be a palindrome?
+
+    public static String gameOfThrones(String s) {
+        // Write your code here
+        char[] charArray = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+        for(char ch : charArray){
+            boolean containsKey = map.containsKey(ch);
+            if(!containsKey){
+                map.put(ch, 1);
+            }else{
+                map.put(ch,1+map.get(ch));
+            }
+        
+        
+        }
+
+        int numberOfOdds = 0;
+
+        for(Integer val : map.values()){
+            if(val % 2 == 1){
+                numberOfOdds++;
+            }
+            if(numberOfOdds > 1){
+                return "NO";
+            }
+        }
+
+
+        return "YES";
+    
+    
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
