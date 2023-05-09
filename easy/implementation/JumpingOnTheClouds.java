2a08694e78eb65430ead5c89139268b55ec9f039
rsmciftci <rsmciftci@gmail.com>
2023-05-09 18:03:23 +0300

    Jumping On the Clouds

---
+++ easy/implementation/JumpingOnTheClouds.java
@@ -0,0 +1,33 @@
+package easy.implementation;
+
+import java.util.*;
+
+public class JumpingOnTheClouds {
+
+    public static int jumpingOnClouds(List<Integer> c) {
+        // Write your code here
+        int index = 0;
+        int jump = -1;
+        while (index < c.size()) {
+
+            boolean canIncreaseBy2 = ((index + 2) < c.size()) && (c.get(index+2) == 0);
+            if (canIncreaseBy2) {
+                index+= 2;
+                jump++;
+            } else {
+                index+= 1;
+                jump++;
+            }
+
+        }
+        return jump;
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(0, 0, 0, 1, 0, 0);
+        System.out.println(jumpingOnClouds(list));
+
+    }
+
+}
